e08de8fe726c65235cf04213a9ba89773961a1bf
vigneshm2 <vicky.mano@gmail.com>
2018-12-19 04:49:11 +0530

    adding chat conversations

--- server/index.js
+++ server/index.js
@@ -12,11 +12,53 @@ app.get('/',function(req,res){
 });
 
 io.on('connection', function(socket){
-	console.log(apimedic)
+	var state = 'FREE'; // FREE -> EXTRA_SYMPT -> DIAGNOSED = FREE
+	var full_symptoms_list = [];
+	io.emit('chat_response', "What are your symptoms");
 	socket.on('chat_message', function(msg){
-		symptoms_list = apimedic.parse_symptoms(msg);
-		console.log(apimedic.get_diagnosis(symptoms_list, data => {console.log(data);}));
-		io.emit('chat_response', msg + "x");
+		if(state == 'FREE'){
+			// get initial symptoms list
+			// TODO age, ..
+			var symptoms_list = apimedic.parse_symptoms(msg);
+			io.emit('chat_response', "Got symptoms " + String(symptoms_list));
+			full_symptoms_list = symptoms_list.slice();
+
+			if(full_symptoms_list.length < 5){
+				// give user suggestions
+				apimedic.get_suggestions(full_symptoms_list, function(data){
+					var response = "Do you have these other symptoms: ";
+					// console.log(data)
+					data.forEach(function(value, index){
+						response = response + String(index+1) + ') ' + value.Name + ' ';
+					})
+					io.emit('chat_response', response)
+					state = 'EXTRA_SYMPT';
+				})
+			} else {
+				apimedic.get_diagnosis(full_symptoms_list, function(data){
+					io.emit('chat_response', "Your diagnosis is : " + JSON.stringify(data))
+					state = 'FREE';
+				})
+
+			}
+			
+		} else if(state == 'EXTRA_SYMPT'){
+			if(msg == "no" || full_symptoms_list.length >= 5){
+				apimedic.get_diagnosis(full_symptoms_list, function(data){
+					io.emit('chat_response', "Your diagnosis is : " + JSON.stringify(data))
+					state = 'FREE';
+				})
+			} else {
+				var symptoms_list = apimedic.parse_symptoms(msg);
+				symptoms_list.forEach(symptom => full_symptoms_list.push(symptom))
+				io.emit('chat_response', "Added symptoms : " + JSON.stringify(symptoms_list))
+				apimedic.get_suggestions(full_symptoms_list, function(data){
+					io.emit('chat_response', "Do you have these other symptoms: " + JSON.stringify(data))
+					state = 'EXTRA_SYMPT';
+				})
+			}
+		}
+		
 	});
 });
 
