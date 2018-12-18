94b248388cb2ee1331a17565887b00ab6d7eb352
Gokulan <gokulan97@gmail.com>
2018-12-19 02:14:34 +0530

    Basic socket working

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 var express = require("express");
 var app = express();
-app.use(express.static(__dirname));
 
-app.get('/*',function(req,res){
-	res.sendFile(__dirname + "/client/index.html");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', function(socket){
+  socket.on('chat_message', function(msg){
+  	//msg is the input received from the user
+  	//Process it and extract key words before performing request to apiMedic
+  });
 });
 
 //create APIs
 
-app.listen(3000);
+http.listen(3000);
 console.log("Server listening on port 3000");
