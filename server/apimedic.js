0231bedab2f20f005899dad0bd5cef0f4744c4ef
vigneshm2 <vicky.mano@gmail.com>
2018-12-19 07:40:32 +0530

    minor chatbot ui changes

--- server/apimedic.js
+++ server/apimedic.js
@@ -5,6 +5,8 @@ var path = require('path');
 
 var SERVER_ROOT = path.join(__dirname, "..");
 
+var base_url = 'https://healthservice.priaid.ch'
+var base_url_sand = 'https://sandbox-healthservice.priaid.ch'
 var API_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InZpZ25lc2htNjI1QGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNDM4MiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAxOC0xMi0xOCIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNTQ1MTg5OTEzLCJuYmYiOjE1NDUxODI3MTN9.2791uFaxWs87weCdNEK8xUItc58akHUInTCR_bYqAQg'
 // sandbox
 var API_KEY_sand = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InZpZ25lc2htNjI1QGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNDM4MiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAxOC0xMi0xOCIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNTQ1MTg3OTY4LCJuYmYiOjE1NDUxODA3Njh9.tgRQy8h4LN0WNgo1N5IajeY-v36exVZdS64YN5f5q6w'
@@ -60,7 +62,7 @@ function get_diagnosis(symptom_names, cb){
     symptom_ids = symptom_names.map(value => get_symptom(value).ID)
     console.log(symptom_ids)
     request.get({
-        url: 'https://sandbox-healthservice.priaid.ch/diagnosis',
+        url: base_url_sand + '/diagnosis',
         qs: {
             symptoms: JSON.stringify(symptom_ids),
             gender: 'male',
@@ -73,12 +75,26 @@ function get_diagnosis(symptom_names, cb){
         cb(JSON.parse(body).slice(0,5));
     });
 }
+https://sandbox-healthservice.priaid.ch/issues/15/info?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InZpZ25lc2htNjI1QGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNDM4MiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvdmVyc2lvbiI6IjIwMCIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGltaXQiOiI5OTk5OTk5OTkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXAiOiJQcmVtaXVtIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9sYW5ndWFnZSI6ImVuLWdiIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9leHBpcmF0aW9uIjoiMjA5OS0xMi0zMSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbWVtYmVyc2hpcHN0YXJ0IjoiMjAxOC0xMi0xOCIsImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNTQ1MTkxMjI4LCJuYmYiOjE1NDUxODQwMjh9.4AroBsI4rDh_-GQxUQEbe2fYMLHWEsV2gKw0yR8HGak&format=json&language=en-gb
+function get_disease_info(disease_id, cb){
+    request.get({
+        url: base_url_sand + '/issues/' + disease_id + '/info',
+        qs: {
+            token: API_KEY_sand,
+            format: 'json',
+            language: 'en-gb'
+        }
+    }, function (error, response, body) {
+        cb(JSON.parse(body));
+    });
+}
+
 
 function get_suggestions(symptom_names, cb){
     symptom_ids = symptom_names.map(value => get_symptom(value).ID)
     // console.log(symptom_ids)
     request.get({
-        url: 'https://sandbox-healthservice.priaid.ch/symptoms/proposed',
+        url: base_url_sand + '/symptoms/proposed',
         qs: {
             symptoms: JSON.stringify(symptom_ids),
             gender: 'male',
@@ -110,10 +126,12 @@ function parse_symptoms(query){
 // console.log(parse_symptoms('cough,hand ,and fever'))
 // console.log(parse_symptoms('I have headache and fever').map(get_symptom_id))
 // get_diagnosis(parse_symptoms('cough, headache and fever'), data => {console.log(data);})
+// get_disease_info(15, data => {console.log(data);})
 
 module.exports = {
     get_diagnosis: get_diagnosis, 
     parse_symptoms: parse_symptoms,
     get_suggestions: get_suggestions,
-    parse_list: parse_list
+    parse_list: parse_list,
+    get_disease_info: get_disease_info
 }
